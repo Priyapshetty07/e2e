c64e5a9dea6494624d68e4ba07a461beec945151
Priyapshetty07 <priyapshetty007@gmail.com>
2022-05-11 15:19:50 +0530

    changed

--- api-learnlytica/db/db.js
+++ api-learnlytica/db/db.js
@@ -35,23 +35,14 @@ require('dotenv').config();
 let mysqlConnect = function () {
 
     let msql= mysql.createConnection({
-
         host: process.env.DB_HOST,
-
         user: process.env.DB_USER,
-
         password: process.env.DB_PASSWORD, //should be yours
-
         database: process.env.DB_NAME,
-
         port: process.env.DB_PORT,
-
         insecureAuth: true
 
     });
     return msql
 };
-
-
-
 module.exports.localConnect = mysqlConnect;
